2b232d9769e6164902a8eb05c75cc6601f40852d
Alec Bakholdin <alecbakholdin@gmail.com>
2022-11-16 17:32:33 -0500

    Can now create new products

--- components/EditProductWizard/EditProductWizard.tsx
+++ components/EditProductWizard/EditProductWizard.tsx
@@ -4,30 +4,50 @@ import {
   Card,
   CardContent,
   CardHeader,
+  CircularProgress,
   Container,
   Grid,
+  Stack,
   TextField,
+  Typography,
 } from "@mui/material";
 import { useDocument } from "react-firebase-hooks/firestore";
 import { Product, productCollection } from "../../types/product";
 import { doc, setDoc } from "@firebase/firestore";
 import ProductSearchAutocomplete from "./ProductSearchAutocomplete";
 import CurrencyTextField from "./CurrencyTextField";
+import { Check, PendingOutlined } from "@mui/icons-material";
 
 export default function EditProductWizard(): React.ReactElement {
+  const [editStatus, setEditStatus] = useState<
+    "pending" | "loading" | "nothing" | "error" | "done"
+  >("nothing");
+
   const [productId, setProductId] = useState<string | null>(null);
   const docReference = productId ? doc(productCollection, productId) : null;
-  const [product, loading, error] = useDocument(docReference);
+  const [product, loading] = useDocument(docReference);
   const [edits, setEdits] = useState<Product | null>(null);
   useEffect(() => {
     if (!loading) {
+      setEditStatus("nothing");
       setEdits(product?.data() ?? null);
     }
   }, [product, loading]);
 
+  const handleEdit = (edit: Partial<Product>) => {
+    setEditStatus("pending");
+    setEdits({ ...edits!, ...edit });
+  };
+
   const handleSubmit = () => {
-    if (docReference && edits) {
-      setDoc(docReference, edits);
+    if (docReference && edits && editStatus == "pending") {
+      setEditStatus("loading");
+      setDoc(docReference, edits)
+        .then(() => setEditStatus("done"))
+        .catch((e) => {
+          setEditStatus("error");
+          console.error(e);
+        });
     }
   };
 
@@ -44,7 +64,7 @@ export default function EditProductWizard(): React.ReactElement {
             <Grid item xs={12}>
               <TextField
                 value={edits?.name || ""}
-                onChange={(e) => setEdits({ ...edits!, name: e.target.value })}
+                onChange={(e) => handleEdit({ name: e.target.value })}
                 disabled={!Boolean(edits)}
                 label={"Name"}
                 fullWidth
@@ -56,7 +76,7 @@ export default function EditProductWizard(): React.ReactElement {
                 disabled={!Boolean(edits)}
                 fullWidth
                 value={edits?.image || ""}
-                onChange={(e) => setEdits({ ...edits!, image: e.target.value })}
+                onChange={(e) => handleEdit({ image: e.target.value })}
               />
             </Grid>
             <Grid item xs={6}>
@@ -66,7 +86,7 @@ export default function EditProductWizard(): React.ReactElement {
                 fullWidth
                 value={edits?.price || 0}
                 onChange={(e) =>
-                  setEdits({ ...edits!, price: parseFloat(e.target.value) })
+                  handleEdit({ price: parseFloat(e.target.value) })
                 }
               />
             </Grid>
@@ -77,12 +97,25 @@ export default function EditProductWizard(): React.ReactElement {
                 fullWidth
                 value={edits?.teamPrice || 0}
                 onChange={(e) =>
-                  setEdits({ ...edits!, teamPrice: parseFloat(e.target.value) })
+                  handleEdit({ teamPrice: parseFloat(e.target.value) })
                 }
               />
             </Grid>
             <Grid item xs={6}>
-              <Button onClick={handleSubmit}>SUBMIT CHANGES</Button>
+              <Stack direction={"row"} alignItems={"center"}>
+                <Button onClick={handleSubmit}>SUBMIT CHANGES</Button>
+                <Typography>
+                  {editStatus === "pending" ? (
+                    <PendingOutlined />
+                  ) : editStatus === "loading" ? (
+                    <CircularProgress size={15} color={"error"} />
+                  ) : editStatus === "done" ? (
+                    <Check />
+                  ) : (
+                    <></>
+                  )}
+                </Typography>
+              </Stack>
             </Grid>
           </Grid>
         </CardContent>
--- pages/store/manage.tsx
+++ pages/store/manage.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Container, Stack } from "@mui/material";
 import EditProductWizard from "../../components/EditProductWizard/EditProductWizard";
 import Typography from "@mui/material/Typography";
+import CreateProductWizard from "../../components/CreateProductWizard";
 
 export default function ManageStore(): React.ReactElement {
   return (
-    <Container maxWidth={"lg"}>
-      <Typography variant={"h4"} gutterBottom>
-        Manage Store
-      </Typography>
-      <Stack>
+    <Container maxWidth={"lg"} sx={{ marginTop: 5 }}>
+      <Stack justifyContent={"center"} spacing={2}>
+        <Typography variant={"h4"} textAlign={"center"}>
+          Manage Store
+        </Typography>
         <EditProductWizard />
+        <CreateProductWizard />
       </Stack>
     </Container>
   );
