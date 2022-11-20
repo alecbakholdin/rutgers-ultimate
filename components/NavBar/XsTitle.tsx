3122648ed0d1f8450628b241ae25011615314d64
Alec Bakholdin <alecbakholdin@gmail.com>
2022-11-20 17:31:30 -0500

    Added logo and favicon, made pages redirect to signIn if not logged in

--- components/NavBar/XsTitle.tsx
+++ components/NavBar/XsTitle.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import AdbIcon from "@mui/icons-material/Adb";
 import Typography from "@mui/material/Typography";
 
 export default function XsTitle(): React.ReactElement {
   return (
     <>
-      <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
       <Typography
         variant="h5"
         noWrap
