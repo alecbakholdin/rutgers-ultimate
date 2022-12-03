09a88335b05836ef16d7130a34e30a64d81f3991
Alec Bakholdin <alecbakholdin@gmail.com>
2022-12-03 16:03:33 -0500

    firestoreConverter now removes undefined values

--- config/firestoreConverter.ts
+++ config/firestoreConverter.ts
@@ -7,15 +7,28 @@ import {
   WithFieldValue,
 } from "@firebase/firestore";
 
+function removeUndefinedValues(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.filter((val) => val !== undefined).map(removeUndefinedValues);
+  } else if (typeof obj === "object") {
+    return Object.fromEntries(
+      Object.entries(obj)
+        .filter(([_, value]) => value !== undefined)
+        .map(([key, value]) => [key, removeUndefinedValues(value)])
+    );
+  }
+  return obj;
+}
+
 export function getFirestoreConverter<
   T extends { id: string; ref?: DocumentReference<T> }
 >(): FirestoreDataConverter<T> {
   return {
     toFirestore(modelObject: WithFieldValue<T>): DocumentData {
       const { id, ref, ...object } = modelObject;
-      return {
-        ...object,
-      };
+      const obj = removeUndefinedValues(object);
+      console.log(obj);
+      return obj;
     },
     fromFirestore(
       snapshot: QueryDocumentSnapshot<T>,
