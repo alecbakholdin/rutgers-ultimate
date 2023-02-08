3075124cfc6e73f7ecec7a19da3a8ab243f449ec
Alec Bakholdin <alecbakholdin@gmail.com>
2023-02-08 13:12:16 -0500

    Added css import back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "/styles/globals.css";
 import type { AppProps } from "next/app";
 import "firebaseui/dist/firebaseui.css";
 import { Box, Button, IconButton, Stack, ThemeProvider } from "@mui/material";
