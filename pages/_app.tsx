8515f581b62432f560d3c20aba9de567b6541a91
Alec Bakholdin <alecbakholdin@gmail.com>
2023-02-08 12:22:20 -0500

    Removed css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "styles/globals.css";
 import type { AppProps } from "next/app";
 import "firebaseui/dist/firebaseui.css";
 import { Box, Button, IconButton, Stack, ThemeProvider } from "@mui/material";
