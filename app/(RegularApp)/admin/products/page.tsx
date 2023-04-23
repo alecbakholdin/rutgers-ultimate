f153715d006a54852cee62b77419e8e6cca9a91d
Alec Bakholdin <alecbakholdin@gmail.com>
2023-04-22 22:06:20 -0400

    Improved color editor to allow creating/deleting colors in /admin/pages

--- app/(RegularApp)/admin/products/page.tsx
+++ app/(RegularApp)/admin/products/page.tsx
@@ -1,14 +1,6 @@
-"use client";
 import React from "react";
 import ProductWizard from "app/(RegularApp)/admin/products/ProductWizard";
-import { Stack } from "@mui/material";
-import ColorWizard from "app/(RegularApp)/admin/products/ColorWizard";
 
 export default function ManageProducts() {
-  return (
-    <Stack spacing={2}>
-      <ProductWizard />
-      <ColorWizard />
-    </Stack>
-  );
+  return <ProductWizard />;
 }
