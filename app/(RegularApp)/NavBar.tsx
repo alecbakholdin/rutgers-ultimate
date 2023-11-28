8a98178130b328a625877e27787686e178e9d4f4
alecbakholdin <alecbakholdin@gmail.com>
2023-11-28 18:49:24 -0500

    removed errant log message and removed home page (redirects to store automatically now)

--- app/(RegularApp)/NavBar.tsx
+++ app/(RegularApp)/NavBar.tsx
@@ -63,10 +63,6 @@ export default function DesktopNavBar({
 
   const pages: NavAction[] = [
     {
-      name: "Home",
-      href: "/",
-    },
-    {
       name: "Store",
       href: "/store",
     },
--- config/firebaseServerApp.ts
+++ config/firebaseServerApp.ts
@@ -1,7 +1,6 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  console.log(process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"));
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
